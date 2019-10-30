0515c50be1fca2fdd6011257be76334ed2f4604f
Marcelo <marcelo.souza@dce.ufpb.br>
2019-10-30 21:19:40 +0000

    Adding function to draw rectangle in image, drowRectangle.

--- src/FindImage.java
+++ src/FindImage.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,15 +11,15 @@ public class FindImage {
 	public static void main(String[] args) {
 		try {
 			BufferedImage image = ImageIO.read(new File("C:/Users/marce/git/Projeto-PCD/src/img/in/Superman.png"));
-			boolean isOnScreen = isOnScreen(image);
+			boolean isOnScreen = isOnImagem(image);
 			System.out.print(isOnScreen);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static boolean isOnScreen(BufferedImage bi) throws IOException {
-		BufferedImage image = ImageIO.read(new File("C:/Users/marce/git/Projeto-PCD/src/img/out/image1_1.png"));
+	private static boolean isOnImagem(BufferedImage bi) throws IOException {
+		BufferedImage image = ImageIO.read(new File("C:/Users/marce/git/Projeto-PCD/src/img/out/image3_4.png"));
 
 		for (int x = 0; x < image.getWidth(); x++) {
 			for (int y = 0; y < image.getHeight(); y++) {
@@ -29,6 +31,7 @@ public class FindImage {
 					for (int b = 0; b < bi.getHeight(); b++) {
 						if (bi.getRGB(a, b) != image.getRGB(k, l)) {
 							invalid = true;
+							drawRectangle(image, a, b);
 							break;
 						} else {
 							l++;
@@ -47,7 +50,16 @@ public class FindImage {
 			}
 		}
 		return false; // If no image is found
+	}
 
+	public static void drawRectangle(BufferedImage image, int x, int y) {
+		Graphics2D g2d = image.createGraphics();
+
+		g2d.setColor(Color.RED);
+
+		g2d.drawRect(x, y, 100, 400);
+
+		g2d.dispose();
 	}
 
 }
