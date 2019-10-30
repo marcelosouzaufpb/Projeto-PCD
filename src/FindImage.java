affd5c6b61121606b4c80b821e752e0df997aae9
Marcelo <marcelo.souza@dce.ufpb.br>
2019-10-30 22:45:11 +0000

    Adding the comments to what has already been done.

--- src/FindImage.java
+++ src/FindImage.java
@@ -21,14 +21,19 @@ public class FindImage {
 		}
 	}
 
+	/*
+	 * The image function receives an image that will be compared with an already
+	 * determined image, checking if there is one image inside the other, all behind
+	 * rgb, returning true or false, for existence.
+	 */
 	private static boolean isOnImagem(BufferedImage bi) throws IOException {
-		BufferedImage image = ImageIO.read(new File("C:/Users/marce/git/Projeto-PCD/src/img/out/image3_4.png"));
+		BufferedImage image = ImageIO.read(new File("C:/Users/marce/git/Projeto-PCD/src/img/out/image6_3.png"));
 
 		for (int x = 0; x < image.getWidth(); x++) {
 			for (int y = 0; y < image.getHeight(); y++) {
-				int c = 0;
-				int d = 0;
-				int e = 0;
+				/*
+				 * int c = 0; int d = 0; int e = 0;
+				 */
 				boolean invalid = false;
 				int k = x, l = y;
 				for (int a = 0; a < bi.getWidth(); a++) {
@@ -39,14 +44,11 @@ public class FindImage {
 							break;
 						} else {
 							l++;
-							c++;
+							// c++;
 						}
-
-						if (c == 1) {
-							d = k;
-							e = l;
-						}
-
+						/*
+						 * if (c == 1) { d = k; e = l; }
+						 */
 					}
 					if (invalid) {
 						break;
@@ -56,7 +58,7 @@ public class FindImage {
 				}
 
 				if (!invalid) {
-					drawRectangle(image, d, e, bi.getWidth(), bi.getHeight());
+					// drawRectangle(image, d, e, bi.getWidth(), bi.getHeight());
 					save(image);
 					return true;
 				}
@@ -65,6 +67,11 @@ public class FindImage {
 		return false; // If no image is found
 	}
 
+	/*
+	 * Draw rectangle function, receives the largest image, the value of x and y,
+	 * the width and height of the smaller image, and returns the larger image,
+	 * marking the smaller image with a rectangle.
+	 */
 	public static void drawRectangle(BufferedImage image, int x, int y, int w, int h) {
 		Graphics2D g2d = image.createGraphics();
 
@@ -75,8 +82,11 @@ public class FindImage {
 		g2d.dispose();
 	}
 
+	/*
+	 * Save function receives an image, which is converted to byte, to be saved in a
+	 * default folder
+	 */
 	public static void save(BufferedImage image) throws IOException {
-
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
 		ImageIO.write(image, "png", baos);
