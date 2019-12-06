e67a855ba90506d17580c13eb9db20875bdffc66
IvoCostajr <ivojuniorbpt@gmail.com>
2019-12-06 18:45:02 +0000

    servidor rodando multithread

--- src/Client.java
+++ src/Client.java
@@ -1,63 +1,91 @@
 
 // A Java program for a Client 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.omg.IOP.TaggedComponentHelper;
+
+import java.awt.List;
 import java.io.*;
 
-public class Client {
-	// initialize socket and input output streams
-	private Socket socket = null;
-	private DataInputStream input = null;
+public class Client implements Runnable{
+
+    private Socket cliente;
+    private DataInputStream input = null;
 	private DataOutputStream out = null;
 	private DataInputStream in = null;
+	
+	
 
-	// constructor to put ip address and port
-	public Client(String address, int port) {
-		// establish a connection
-		try {
-			socket = new Socket(address, port);
-			System.out.println("Connected");
+    public Client(Socket cliente){
+        this.cliente = cliente;
+    }
 
-			// takes input from terminal
-			input = new DataInputStream(System.in);
+    public static void main(String args[]) throws UnknownHostException, IOException {
 
-			// sends output to the socket
-			out = new DataOutputStream(socket.getOutputStream());
-			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        // para se conectar ao servidor, cria-se objeto Socket.
+        // O primeiro parâmetro é o IP ou endereço da máquina que
+        // se quer conectar e o segundo é a porta da aplicação.
+        // Neste caso, usa-se o IP da máquina local (127.0.0.1)
+        // e a porta da aplicação ServidorDeEco (12345).
+        Socket socket = new Socket("127.0.0.1", 12345);
 
-		} catch (UnknownHostException u) {
-			System.out.println(u);
-		} catch (IOException i) {
-			System.out.println(i);
-		}
+        /*Cria um novo objeto Cliente com a conexão socket para que seja executado em um novo processo.
+        Permitindo assim a conexão de vário clientes com o servidor.*/
+        Client c = new Client(socket);
+        Thread t = new Thread(c);
+        t.start();
+        System.out.println(t);
+        
+    }
 
-		// string to read message from input
-		String line = "";
+    public void run() {
+        
+    	// establish a connection
+    			try {
+    				
+    				System.out.println("Connected");
 
-		// keep reading until "Over" is input
-		while (!line.equals("OVER")) {
-			try {
-				line = input.readLine();
-				line = line.toUpperCase();
-				out.writeUTF(line);
-				
-				System.out.println(in.readUTF());
+    				// takes input from terminal
+    				input = new DataInputStream(System.in);
 
-			} catch (IOException i) {
-				System.out.println(i);
-			}
-		}
+    				// sends output to the socket
+    				out = new DataOutputStream(cliente.getOutputStream());
+    				in = new DataInputStream(new BufferedInputStream(cliente.getInputStream()));
 
-		// close the connection
-		try {
-			input.close();
-			out.close();
-			socket.close();
-		} catch (IOException i) {
-			System.out.println(i);
-		}
-	}
+    			} catch (UnknownHostException u) {
+    				System.out.println(u);
+    			} catch (IOException i) {
+    				System.out.println(i);
+    			}
 
-	public static void main(String args[]) {
-		Client client = new Client("127.0.0.1", 12345);
-	}
+    			// string to read message from input
+    			String line = "";
+
+    			// keep reading until "Over" is input
+    			while (!line.equals("OVER")) {
+    				try {
+    					line = input.readLine();
+    					line = line.toUpperCase();
+    					out.writeUTF(line);
+
+    					System.out.println(in.readUTF());
+    				
+    		
+
+    				} catch (IOException i) {
+    					System.out.println(i);
+    				}
+    			}
+
+    			// close the connection
+    			try {
+    				input.close();
+    				out.close();
+    				cliente.close();
+    			} catch (IOException i) {
+    				System.out.println(i);
+    			}
+    		}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,75 +1,135 @@
 
 // A Java program for a Server 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.net.ssl.SSLContext;
+
+import RMI.Cliente;
+
 import java.io.*;
 
-public class Server {
-	// initialize socket and input stream
-	private Socket socket = null;
-	private ServerSocket server = null;
-	private DataInputStream in = null;
+public class Server implements Runnable {
+	public Socket cliente;
 	private DataOutputStream out = null;
-	
+	private DataInputStream in = null;
+	private  ArrayList<Worker> lista = new ArrayList<Worker>();
+	private  static int cont=0;
 
-	// constructor with port
-	public Server(int port) {
-		// starts server and waits for a connection
+	public Server(Socket cliente) {
+		this.cliente = cliente;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		// Cria um socket na porta 12345
+		ServerSocket servidor = new ServerSocket(12345);
+		System.out.println("Porta 12345 aberta!");
+
+		// Aguarda alguém se conectar. A execução do servidor
+		// fica bloqueada na chamada do método accept da classe
+		// ServerSocket. Quando alguém se conectar ao servidor, o
+		// método desbloqueia e retorna com um objeto da classe
+		// Socket, que é uma porta da comunicação.
+		System.out.println("Aguardando conexão do cliente...");
+
+		while (true) {
+			Socket cliente = servidor.accept();
+			// Cria uma thread do servidor para tratar a conexão
+			Server tratamento = new Server(cliente);
+			Thread t = new Thread(tratamento);
+			// Inicia a thread para o cliente conectado
+			t.start();
+			cont++;
+			System.out.println(cont);
+		}
+	}
+
+	/*
+	 * A classe Thread, que foi instancia no servidor, implementa Runnable. Então
+	 * você terá que implementar sua lógica de troca de mensagens dentro deste
+	 * método 'run'.
+	 */
+	public void run() {
 		try {
-			server = new ServerSocket(port);
-			System.out.println("Server started");
-
-			System.out.println("Waiting for a client ...");
-
-			socket = server.accept();
-			System.out.println("Client accepted");
-
-			// takes input from the client socket
-			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-			out = new DataOutputStream(socket.getOutputStream());
-             
+			out = new DataOutputStream(cliente.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(cliente.getInputStream()));
 			String line = "";
-			
 
 			// reads message from client until "Over" is sent
 			while (!line.equals("OVER")) {
 				try {
 					line = in.readUTF();
-
-					System.out.println(line);
 					if (line.equals("BTN90")) {
-						out.writeUTF("ADD TRAB 90");
-						
-					} else if (line.equals("BTN180")) {
-						out.writeUTF("ADD TRAB 180");
+						System.out.println("ADD TRAB 90");
+						out.writeUTF("ok");
 
-					} else if (line.equals("BTNSIMPLES")) {
-						out.writeUTF("ADD TRAB SIMPLES");
-						
+					} else if (line.equals("btn180")) {
+						System.out.println("ADD TRAB 180");
+
+					} else if (line.equals("btnSIMPLES")) {
+						System.out.println("ADD TRAB SIMPLES");
+
 					} else if (line.equals("BTNCLIENTE")) {
-						out.writeUTF("Cliente add...");
+						System.out.println("Cliente add...");
 						View view = new View();
 						view.show();
-					
-						
 					} else {
-						out.writeUTF("Não existe esse tipo de usuario...");
+						System.out.println("Não existe esse tipo de usuario...");
+						out.writeUTF("nao");
 					}
 
-				} catch (IOException i) {
+				}
+
+				catch (IOException i) {
 					System.out.println(i);
 				}
 			}
 			System.out.println("Closing connection");
 
-			// close connection
-			socket.close();
-			in.close();
-		} catch (IOException i) {
-			System.out.println(i);
+		} catch (
+
+		IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		// close connection
+		try {
+			cliente.close();
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	public static void main(String args[]) {
-		Server server = new Server(12345);
-	}
 }
+
+/*
+ * try { in = new DataInputStream(new
+ * BufferedInputStream(cliente.getInputStream())); out = new
+ * DataOutputStream(cliente.getOutputStream()); String line = "";
+ * 
+ * // reads message from client until "Over" is sent while
+ * (!line.equals("OVER")) {
+ * 
+ * line = in.readUTF(); if (line.equals("BTN90")) {
+ * System.out.println("ADD TRAB 90"); out.writeUTF("ok");
+ * 
+ * } else if (line.equals("btn180")) { System.out.println("ADD TRAB 180");
+ * 
+ * } else if (line.equals("btnSIMPLES")) {
+ * System.out.println("ADD TRAB SIMPLES");
+ * 
+ * } else if (line.equals("BTNCLIENTE")) { System.out.println("Cliente add...");
+ * } else { System.out.println("Não existe esse tipo de usuario...");
+ * out.writeUTF("ok"); }
+ * 
+ * } System.out.println("Closing connection");
+ * 
+ * // close connection cliente.close(); in.close(); } catch (IOException i) {
+ * System.out.println(i); } }
+ */
